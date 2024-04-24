b61cc483d0451fda4e5d42eb7c98ab69c99371f9
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-24 09:48:46 +0200

    chore: try out CBOR response / transform

--- simulator.go
+++ simulator.go
@@ -16,6 +16,7 @@ import (
 	"github.com/plgd-dev/go-coap/v3/message"
 	"github.com/plgd-dev/go-coap/v3/message/codes"
 	"github.com/plgd-dev/go-coap/v3/message/pool"
+	udpClient "github.com/plgd-dev/go-coap/v3/udp/client"
 
 	"crypto/ecdsa"
 	"log"
@@ -78,6 +79,69 @@ func main() {
 	}
 	log.Println("DeviceID:", *deviceId)
 
+	token, err := createJWTToken(*privateKeyPath, *deviceId)
+	if err != nil {
+		log.Fatalf("Failed to create JWT token: %v", err)
+	}
+
+	log.Println("JWT Token:", token)
+
+	// Connect to a DTLS server
+	co, err := dtls.Dial("coap.nrfcloud.com:5684", &piondtls.Config{
+		InsecureSkipVerify:    true,
+		ConnectionIDGenerator: piondtls.OnlySendCIDGenerator(),
+	})
+	util.Check(err)
+	defer func() {
+		util.Check(co.Close())
+	}()
+
+	log.Println("Connected.")
+
+	// Authenticate
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	resp, err := co.Post(ctx, "/auth/jwt", message.TextPlain, strings.NewReader(token))
+	check(err)
+	checkResponse(resp, codes.Created)
+	// assertEquals(Code.C201_CREATED, resp.code)
+	// assertTrue(resp.options().maxAge > 0)
+
+	log.Println("Authenticated.")
+
+	getState(co)
+	publishSenML(co)
+}
+
+func checkResponse(resp *pool.Message, expected codes.Code) {
+	if resp.Code() != expected {
+		panic(fmt.Sprintf(`Request failed: %d`, resp.Code()))
+	}
+}
+
+func getState(co *udpClient.Conn) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Get the state
+	// options := []message.Option{
+	// 	{ID: message.Accept, Value: []byte(message.TextPlain.String())},
+	// 	{ID: message.URIQuery, Value: []byte("?transform=pairing")},
+	// }
+	// stateResp, err := co.Get(ctx, "/state", options...)
+	stateResp, err := co.Get(ctx, "/state")
+	check(err)
+	checkResponse(stateResp, codes.Content)
+	data, err := io.ReadAll(stateResp.Body())
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("< State: %s\n", data)
+}
+
+func publishSenML(co *udpClient.Conn) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 	// Read JSON from stdin
 	senMLJSON, err := io.ReadAll(os.Stdin)
 	if err != nil {
@@ -95,46 +159,6 @@ func main() {
 		panic(err) // handle the error
 	}
 
-	token, err := createJWTToken(*privateKeyPath, *deviceId)
-	if err != nil {
-		log.Fatalf("Failed to create JWT token: %v", err)
-	}
-
-	log.Println("JWT Token:", token)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-
-	// Connect to a DTLS server
-	co, err := dtls.Dial("coap.nrfcloud.com:5684", &piondtls.Config{
-		InsecureSkipVerify:    true,
-		ConnectionIDGenerator: piondtls.OnlySendCIDGenerator(),
-	})
-	util.Check(err)
-	defer func() {
-		util.Check(co.Close())
-	}()
-
-	log.Println("Connected.")
-
-	// Authenticate
-
-	resp, err := co.Post(ctx, "/auth/jwt", message.TextPlain, strings.NewReader(token))
-	check(err)
-	checkResponse(resp, codes.Created)
-	// assertEquals(Code.C201_CREATED, resp.code)
-	// assertTrue(resp.options().maxAge > 0)
-
-	// Get the state
-	stateResp, err := co.Get(ctx, "/state")
-	check(err)
-	checkResponse(stateResp, codes.Content)
-	data, err := io.ReadAll(stateResp.Body())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("< State: %s\n", data)
-
 	// encode the normalized SenML Pack to CBOR
 	dataOut, err := senMLCodec.EncodeCBOR(senMLPayload)
 	if err != nil {
@@ -149,11 +173,4 @@ func main() {
 	rawResp, err := co.Post(ctx, "/msg/d2c/raw", message.AppCBOR, bytes.NewReader(dataOut))
 	check(err)
 	checkResponse(rawResp, codes.Created)
-
-}
-
-func checkResponse(resp *pool.Message, expected codes.Code) {
-	if resp.Code() != expected {
-		panic(fmt.Sprintf(`Request failed: %d`, resp.Code()))
-	}
 }
